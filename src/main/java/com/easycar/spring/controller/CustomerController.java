20b7f80d6dca904dfc6ab6f914c9ebb05e8a6144
Shanil Miranda <shanilmiranda24@gmail.com>
2020-11-14 11:59:06 +0530

    adding the configuration

---
+++ src/main/java/com/easycar/spring/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.easycar.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customer")
+public class CustomerController {
+
+    @GetMapping()
+    public String m1(){
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/easycar/spring/main/WebRootConfig.java
@@ -0,0 +1,7 @@
+package com.easycar.spring.main;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+}
